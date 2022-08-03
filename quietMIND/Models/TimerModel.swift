1d25cc1189169dd4eb9364028c633272daf6b7c0
Ameen Mustafa <amnmustafa15@gmail.com>
2022-08-03 14:29:15 -0600

    Update TimerModel.swift
    Added Lenny and Carl achieving enlightenment mp3

--- quietMIND/Models/TimerModel.swift
+++ quietMIND/Models/TimerModel.swift
@@ -19,6 +19,6 @@ struct TimerModel {
     var paused: Bool = false
     
     var times = [1, 2, 3, 4, 5, 10, 15, 20, 30, 45, 60]
-    var soundsLight = ["None", "Light Rain", "Spring Forest", "Relaxing Beach" ]
-    var soundsDark = ["None", "Creepy Kids", "The Forbidden Forest", "Conjuring The Dark One"]
+    var soundsLight = ["None", "Light Rain", "Spring Forest", "Relaxing Beach", "Lenny And Carl" ]
+    var soundsDark = ["None", "Creepy Kids", "The Forbidden Forest", "Conjuring The Dark One", "Lenny And Carl"]
 }
